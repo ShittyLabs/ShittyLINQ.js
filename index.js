d2a768663944e65948231cca2b6f8a62fd7d13a7
jmeline <jacob.meline@gmail.com>
2018-10-03 22:48:17 -0600

    Adding takewhile

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const Aggregate = require('./src/Aggregate');
 const Select = require('./src/Select');
 const Where = require('./src/Where');
+const TakeWhile = require('./src/TakeWhile');
 
 const bindAll = function() {
   Array.prototype.Aggregate = Aggregate;
   Array.prototype.Select = Select;
   Array.prototype.Where = Where;
+  Array.prototype.TakeWhile = TakeWhile;
 };
 
 module.exports = bindAll;
