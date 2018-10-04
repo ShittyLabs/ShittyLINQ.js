feb5fa9bfab7609fb3fd719d4d98f4d991370d94
Suchart Chayangsu <suchart.chayangsu@thomsonreuters.com>
2018-10-04 11:52:40 +0700

    Add Concat method

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Aggregate = require('./src/Aggregate');
+const Concat = require('./src/Concat');
 const Select = require('./src/Select');
 const Where = require('./src/Where');
 
 const bindAll = function() {
   Array.prototype.Aggregate = Aggregate;
+  Array.prototype.Concat = Concat;
   Array.prototype.Select = Select;
   Array.prototype.Where = Where;
 };
