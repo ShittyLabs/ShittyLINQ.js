50f94bdc4a68559dcf8fefd41eece4c2ea08eb07
Mahasak Pijittum <mpijittum@fb.com>
2018-10-04 11:32:50 +0700

    Add union extension method

--- index.d.ts
+++ index.d.ts
@@ -3,4 +3,5 @@ interface Array<T> {
   Aggregate<T>(accum: (memo: T, val: T) => T): T;
   Select<T, U>(xform: (x: T) => U): Array<U>;
   Where<T>(predicate: (X: T) => boolean): Array<T>;
+  Union<T>(adder: Array<T>): Array<T>;
 }
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const Aggregate = require('./src/Aggregate');
 const Select = require('./src/Select');
 const Where = require('./src/Where');
+const Union = require('./src/Union');
 
 const bindAll = function() {
   Array.prototype.Aggregate = Aggregate;
   Array.prototype.Select = Select;
   Array.prototype.Where = Where;
+  Array.prototype.Union = Union;
 };
 
 module.exports = bindAll;
