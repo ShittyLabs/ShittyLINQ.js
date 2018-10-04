08dd8e750c5abe8e6345724d06020393962dceb4
Ashok Subedi <ashok.subedi@SPI.COM>
2018-10-04 11:25:15 +0545

    Add Except

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const Aggregate = require('./src/Aggregate');
 const Select = require('./src/Select');
 const Where = require('./src/Where');
+const Except = require('./src/Except');
 
 const bindAll = function() {
   Array.prototype.Aggregate = Aggregate;
   Array.prototype.Select = Select;
   Array.prototype.Where = Where;
+  Array.prototype.Except = Except;
 };
 
 module.exports = bindAll;
