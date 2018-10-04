bccf17cbbf227b38021195806e503e298b91f77f
jmeline <jacob.meline@gmail.com>
2018-10-03 22:33:00 -0600

    Adding Last Linq example + tests

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const Aggregate = require('./src/Aggregate');
 const Select = require('./src/Select');
 const Where = require('./src/Where');
+const Last = require('./src/Last');
 
 const bindAll = function() {
   Array.prototype.Aggregate = Aggregate;
   Array.prototype.Select = Select;
   Array.prototype.Where = Where;
+  Array.prototype.Last = Last;
 };
 
 module.exports = bindAll;
