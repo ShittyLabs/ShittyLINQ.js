78942ee17b74a0f364bd4d6037fc20c8d97f16f1
jmeline <jacob.meline@gmail.com>
2018-10-03 23:17:57 -0600

    Adding All

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const Aggregate = require('./src/Aggregate');
 const Select = require('./src/Select');
 const Where = require('./src/Where');
+const All = require('./src/All');
 
 const bindAll = function() {
   Array.prototype.Aggregate = Aggregate;
   Array.prototype.Select = Select;
   Array.prototype.Where = Where;
+  Array.prototype.All = All;
 };
 
 module.exports = bindAll;
