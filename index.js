d62d8b68c1042eaaeefa1ed956abfcc8bfad8d80
cruggles <cruggles@iastate.edu>
2018-10-03 23:11:45 -0500

    pr comments

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const Aggregate = require('./src/Aggregate');
 const Select = require('./src/Select');
 const Where = require('./src/Where');
+const Prepend = require('./src/Prepend');
 
 const bindAll = function() {
   Array.prototype.Aggregate = Aggregate;
   Array.prototype.Select = Select;
   Array.prototype.Where = Where;
+  Array.prototype.Prepend = Prepend;
 };
 
 module.exports = bindAll;
