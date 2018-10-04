11a839b908598f480a7d09757eb1a154fcf3105c
Vincent Teng <vincentt117@gmail.com>
2018-10-04 00:58:53 -0400

    Added OrderBy test for string sorting

--- test/OrderBy.spec.js
+++ test/OrderBy.spec.js
@@ -1,7 +1,13 @@
 const expect = require('chai').expect;
 require('..')();
 describe('Array#prototype#Reverse', function () {
-    
+
+    it('Should return sorted string elements', function () {
+        const inputAlpha = ["a", "c", "d", "b"];
+        const expectedAlpha = ["a", "b", "c", "d"];
+        const actualAlpha = inputAlpha.OrderBy();
+        expect(actualAlpha).to.eql(expectedAlpha);
+    });
     it('Should return sorted consectutive elements', function () {
         const inputRand = [1, 3, 2, 5, 4];
         const expectedRand = [1, 2, 3, 4, 5];
@@ -9,10 +15,10 @@ describe('Array#prototype#Reverse', function () {
         expect(actualRand).to.eql(expectedRand);
     });
     it('Should return sorted non-consectutive elements', function () {
-        const inputRandNonConsec = [1,10, 20, 4, 11];
-        const expectedRandNonConsec  = [1, 4, 10, 11, 20];
-        const actualRandNonConsec  = inputRandNonConsec .OrderBy();
-        expect(actualRandNonConsec ).to.eql(expectedRandNonConsec );
+        const inputRandNonConsec = [1, 10, 20, 4, 11];
+        const expectedRandNonConsec = [1, 4, 10, 11, 20];
+        const actualRandNonConsec = inputRandNonConsec.OrderBy();
+        expect(actualRandNonConsec).to.eql(expectedRandNonConsec);
     });
     it('Should return sorted array the same elements', function () {
         const inputAllSame = [1, 1, 1];
