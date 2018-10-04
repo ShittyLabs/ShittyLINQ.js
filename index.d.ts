d1560278258c97a4ee1ca6710ee36b6fbe1c881c
Estevan Jantsk <estevan.jantsk@gmail.com>
2018-10-04 10:36:52 -0300

    Add Count method

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,8 @@
 interface Array<T> {
   Aggregate<T, U>(seed: U, accum: (memo: U, val: T) => U): U;
   Aggregate<T>(accum: (memo: T, val: T) => T): T;
+  Count<T>(): number;
+  Count<T>(predicate: (X: T) => boolean): number;
   Select<T, U>(xform: (x: T) => U): Array<U>;
   Where<T>(predicate: (X: T) => boolean): Array<T>;
 }
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Aggregate = require('./src/Aggregate');
+const Count = require('./src/Count');
 const Select = require('./src/Select');
 const Where = require('./src/Where');
 
 const bindAll = function() {
   Array.prototype.Aggregate = Aggregate;
+  Array.prototype.Count = Count;
   Array.prototype.Select = Select;
   Array.prototype.Where = Where;
 };
