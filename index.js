d2bcb3823c49de7258554ac5b25e208cf0c1ba38
Mat Malkowski <monkey3310@gmail.com>
2018-10-05 23:06:59 +0700

    Adding ToDictionary

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Last = require('./src/Last');
 const Reverse = require('./src/Reverse');
 const Select = require('./src/Select');
 const Where = require('./src/Where');
+const ToDictionary = require('./src/ToDictionary');
 
 const bindAll = function() {
   Array.prototype.Aggregate = Aggregate;
@@ -10,6 +11,7 @@ const bindAll = function() {
   Array.prototype.Reverse = Reverse;
   Array.prototype.Select = Select;
   Array.prototype.Where = Where;
+  Array.prototype.ToDictionary = ToDictionary;
 };
 
 module.exports = bindAll;
