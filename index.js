5bceab1c0dbb735b75621e54f84ce585e57d86b7
Gary <senjuninja@yahoo.com>
2018-10-04 23:04:33 -0500

    Adds SkipWhile

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const Aggregate = require('./src/Aggregate');
 const Select = require('./src/Select');
 const Where = require('./src/Where');
-
+const SkipWhile = require('./src/SkipWhile');
 const bindAll = function() {
-  Array.prototype.Aggregate = Aggregate;
-  Array.prototype.Select = Select;
-  Array.prototype.Where = Where;
+    Array.prototype.Aggregate = Aggregate;
+    Array.prototype.Select = Select;
+    Array.prototype.Where = Where;
+    Array.prototype.SkipWhile = SkipWhile;
 };
 
 module.exports = bindAll;
