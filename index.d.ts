6a82c45a9d2a91b4e544b1793310e5bd3684cbda
cooray <corycarlson209@gmail.com>
2018-10-06 12:34:13 -0500

    Added First

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,7 @@ interface Array<T> {
   Aggregate<T>(accum: (memo: T, val: T) => T): T;
   All<T>(predicate: (X: T) => boolean): boolean;
   ElementAt<T>(index: number): T;
+  First<T>(predicate: boolean): T;
   Last<T>(): T;
   Prepend<T>(element: T): Array<T>;
   Reverse<T>(): Array<T>;
---
+++ src/First.js
@@ -0,0 +1,15 @@
+/**
+ * @template T
+ * @param {((x: T) => boolean)} predicate
+ * @returns {T}
+ */
+function First(predicate) {
+  if (!this.length) throw 'Sequence contains no elements';
+  if (predicate) {
+    let val = this.find(predicate);
+    if (!val) throw 'Sequence contains no matching element';
+    return this.find(val => predicate(val));
+  }
+  return this[0];
+}
+module.exports = First;
