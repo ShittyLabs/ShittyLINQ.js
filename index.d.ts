4e729224289105360d23291c0ff41e8b53288c7f
cooray <corycarlson209@gmail.com>
2018-10-06 10:57:48 -0500

    Add Max and tests, BOYYYYYYY

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,8 @@ interface Array<T> {
   Aggregate<T>(accum: (memo: T, val: T) => T): T;
   ElementAt<T>(index: number): T;
   Last<T>(): T;
+  Max<number>(): number;
+  Max<T>(xform: (x: T) => number): T;
   Prepend<T>(element: T): Array<T>;
   Reverse<T>(): Array<T>;
   Select<T, U>(xform: (x: T) => U): Array<U>;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Aggregate = require('./src/Aggregate');
 const Last = require('./src/Last');
+const Max = require('./src/Max');
 const Prepend = require('./src/Prepend');
 const Reverse = require('./src/Reverse');
 const Select = require('./src/Select');
@@ -11,6 +12,7 @@ const ElementAt = require('./src/ElementAt');
 const bindAll = function() {
   Array.prototype.Aggregate = Aggregate;
   Array.prototype.Last = Last;
+  Array.prototype.Max = Max;
   Array.prototype.Prepend = Prepend;
   Array.prototype.Reverse = Reverse;
   Array.prototype.Select = Select;
