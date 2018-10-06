2d90795b2674fa4e9a362bd454bb5859a18d401c
Johnny Reina <johnny.r.reina@gmail.com>
2018-10-05 21:34:54 -0500

    Adds GroupJoin

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,21 @@
+type IEqualityComparer<T> = <T>(a: T, b: T) => boolean;
+
 interface Array<T> {
   Aggregate<T, U>(seed: U, accum: (memo: U, val: T) => U): U;
   Aggregate<T>(accum: (memo: T, val: T) => T): T;
+  GroupJoin<T, U, V, K>(
+    inner: Array<U>,
+    outerKeySelector: ((x: T) => K),
+    innerKeySelector: ((x: T) => K),
+    resultSelector: ((x: T, y: U) => V)
+  );
+  GroupJoin<T, U, V, K>(
+    inner: Array<U>,
+    outerKeySelector: ((x: T) => K),
+    innerKeySelector: ((x: T) => K),
+    resultSelector: ((x: T, y: U) => V),
+    equalityComparer: IEqualityComparer<T>
+  );
   Select<T, U>(xform: (x: T) => U): Array<U>;
   Where<T>(predicate: (X: T) => boolean): Array<T>;
   Last<T>(): T;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Aggregate = require('./src/Aggregate');
+const GroupJoin = require('./src/GroupJoin');
 const Last = require('./src/Last');
 const Reverse = require('./src/Reverse');
 const Select = require('./src/Select');
@@ -6,6 +7,7 @@ const Where = require('./src/Where');
 
 const bindAll = function() {
   Array.prototype.Aggregate = Aggregate;
+  Array.prototype.GroupJoin = GroupJoin;
   Array.prototype.Last = Last;
   Array.prototype.Reverse = Reverse;
   Array.prototype.Select = Select;
