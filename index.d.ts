2de9a25cc0d3fc0d1a7b90918d70624ba5cb1936
Johnny Reina <johnny.r.reina@gmail.com>
2018-10-06 09:17:05 -0500

    Alpha sort and probably causing a merge conflict

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 interface Array<T> {
   Aggregate<T, U>(seed: U, accum: (memo: U, val: T) => U): U;
   Aggregate<T>(accum: (memo: T, val: T) => T): T;
+  All<T>(predicate: (X: T) => boolean): boolean;
   Select<T, U>(xform: (x: T) => U): Array<U>;
   Where<T>(predicate: (X: T) => boolean): Array<T>;
-  All<T>(predicate: (X: T) => boolean): boolean;
 }
