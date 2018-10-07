6544d9ce63c6d54b83ae179b2c354c9305d6e587
Suchart Chayangsu <sicker27@hotmail.com>
2018-10-07 07:58:45 +0700

    Add Concat method (#44)

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,7 @@ interface Array<T> {
   Aggregate<T, U>(seed: U, accum: (memo: U, val: T) => U): U;
   Aggregate<T>(accum: (memo: T, val: T) => T): T;
   All<T>(predicate: (X: T) => boolean): boolean;
+  Concat<T>(list: Array<T>)  : Array<T>;
   ElementAt<T>(index: number): T;
   First<T>(): T;
   First<T>(predicate: (x: T) => boolean): T;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Aggregate = require('./src/Aggregate');
 const All = require('./src/All');
+const Concat = require('./src/Concat');
 const ElementAt = require('./src/ElementAt');
 const First = require('./src/First');
 const Last = require('./src/Last');
@@ -7,13 +8,14 @@ const Prepend = require('./src/Prepend');
 const Reverse = require('./src/Reverse');
 const Select = require('./src/Select');
 const TakeWhile = require('./src/TakeWhile');
+const ToDictionary = require('./src/ToDictionary');
 const Union = require('./src/Union');
 const Where = require('./src/Where');
-const ToDictionary = require('./src/ToDictionary');
 
 const bindAll = function() {
   Array.prototype.Aggregate = Aggregate;
   Array.prototype.All = All;
+  Array.prototype.Concat = Concat;
   Array.prototype.ElementAt = ElementAt;
   Array.prototype.First = First;
   Array.prototype.Last = Last;
@@ -21,9 +23,9 @@ const bindAll = function() {
   Array.prototype.Reverse = Reverse;
   Array.prototype.Select = Select;
   Array.prototype.TakeWhile = TakeWhile;
+  Array.prototype.ToDictionary = ToDictionary;
   Array.prototype.Union = Union;
   Array.prototype.Where = Where;
-  Array.prototype.ToDictionary = ToDictionary;
 };
 
 module.exports = bindAll;
