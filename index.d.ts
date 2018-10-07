ffce252ff9d4c9e9a3443781037e74e1664f8ae8
Johnny Reina <johnny.r.reina@gmail.com>
2018-10-06 20:10:51 -0500

    Adds types for ToDictionary

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ interface Array<T> {
   Aggregate<T, U>(seed: U, accum: (memo: U, val: T) => U): U;
   Aggregate<T>(accum: (memo: T, val: T) => T): T;
   All<T>(predicate: (X: T) => boolean): boolean;
-  Concat<T>(list: Array<T>)  : Array<T>;
+  Concat<T>(list: Array<T>): Array<T>;
   ElementAt<T>(index: number): T;
   First<T>(): T;
   First<T>(predicate: (x: T) => boolean): T;
@@ -11,6 +11,14 @@ interface Array<T> {
   Reverse<T>(): Array<T>;
   Select<T, U>(xform: (x: T) => U): Array<U>;
   TakeWhile<T>(predicate: (X: T) => boolean): Array<T>;
+  ToDictionary<T, U, V>(
+    keySelector: ((x: T) => U),
+    elementSelector: ((x: T) => V)
+  ): Dictionary<U, V>;
   Union<T>(adder: Array<T>): Array<T>;
   Where<T>(predicate: (X: T) => boolean): Array<T>;
+}
+
+interface Dictionary<U, V> {
+  [x: U]: V;
 }
