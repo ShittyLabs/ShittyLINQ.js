d6f90893a47707571c61aa2e70a8b7f2d9a58190
Johnny Reina <johnny.r.reina@gmail.com>
2018-10-06 20:57:56 -0500

    Adds GroupJoin (#58)

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
+type IEqualityComparer<T> = <T>(a: T, b: T) => boolean;
+
 interface Array<T> {
   Aggregate<T, U>(seed: U, accum: (memo: U, val: T) => U): U;
   Aggregate<T>(accum: (memo: T, val: T) => T): T;
@@ -8,6 +10,19 @@ interface Array<T> {
   ElementAt<T>(index: number): T;
   First<T>(): T;
   First<T>(predicate: (x: T) => boolean): T;
+  GroupJoin<T, U, V, K>(
+    inner: Array<U>,
+    outerKeySelector: ((x: T) => K),
+    innerKeySelector: ((x: T) => K),
+    resultSelector: ((x: T, y: U) => V)
+  );
+  GroupJoin<T, U, V, K>(
+    inner: Array<U>,
+    outerKeySelector: ((x: T) => K),
+    innerKeySelector: ((x: T) => K),
+    resultSelector: ((x: T, y: U) => V),
+    equalityComparer: IEqualityComparer<T>
+  );
   Last<T>(): T;
   Prepend<T>(element: T): Array<T>;
   Reverse<T>(): Array<T>;
