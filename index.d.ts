a098f52fb9920e42286962448cc3763a4e1d21b4
Johnny Reina <johnny.r.reina@gmail.com>
2018-10-09 22:41:26 -0500

    v0.0.3 (#71)
    * Renames map to Select.
    Adds index.
    
    * Adds README.
    Adds prettier, lint-staged, and husky.
    
    * Adds singleQuote to Prettier config
    
    * Comma not needed
    
    * Adds contribution guide and code of conduct
    
    * Changes capitalization of Select
    
    * Adds Where and Aggregate
    
    * Adds type declaration file.
    Adds type instruction to contribution guide.
    
    * Adds license and readme info from og package
    
    * add reverse function
    
    * Adding Last Linq example + tests (#38)
    
    * Adds travis config
    
    * Adds *.ts to lint patterns
    
    * Adds Prepend method (#35)
    
    * Add Union extension method (#39)
    
    * Adds TakeWhile (#42)
    
    * Adding ElementAt method (#54)
    
    * Adding All (#47)
    
    * Adding ToDictionary (#57)
    
    * Add First method (#63)
    
    * Add Concat method (#44)
    
    * Add Sum method (#34)
    
    * Add Count method (#36)
    
    * Add Count method (#51)
    
    * Add Array.Empty (#53)
    
    * Adds SkipWhile (#55)
    
    * Adds GroupJoin (#58)
    
    * Add max (#60)
    
    * Added Min (#61)
    
    * Adds Any (#62)
    
    * Adds FirstOrDefault (#64)
    
    * Adds Contains Function (#40)
    
    * Adds Repeat (#20) (#68)
    
    * Add Single method (#45)
    
    * Adds Average (#46)
    
    * Adds types for ToDictionary (#65)
    
    * 0.0.2
    
    * Cleanup (#70)
    
    * Updates readme with build status and install instructions.
    
    * Cleans up some typings
    
    * Removes extra period from takewhile spec file name
    
    * 0.0.3

--- index.d.ts
+++ index.d.ts
@@ -3,19 +3,19 @@ type IEqualityComparer<T> = <T>(a: T, b: T) => boolean;
 interface Array<T> {
   Aggregate<T, U>(seed: U, accum: (memo: U, val: T) => U): U;
   Aggregate<T>(accum: (memo: T, val: T) => T): T;
-  All<T>(predicate: (X: T) => boolean): boolean;
+  All<T>(predicate: (x: T) => boolean): boolean;
   Any<T>(): boolean;
-  Any<T>(predicate: (X: T) => boolean): boolean;
-  Average<number>(): number;
+  Any<T>(predicate: (x: T) => boolean): boolean;
+  Average(): number;
   Average<T>(xform: (x: T) => number): number;
   Concat<T>(list: Array<T>): Array<T>;
   Count<T>(): number;
-  Count<T>(predicate: (X: T) => boolean): number;
+  Count<T>(predicate: (x: T) => boolean): number;
   ElementAt<T>(index: number): T;
   First<T>(): T;
   First<T>(predicate: (x: T) => boolean): T;
   FirstOrDefault<T>(): T;
-  FirstOrDefault<T>(pedicate: (x: T) => boolean): T;
+  FirstOrDefault<T>(predicate: (x: T) => boolean): T;
   GroupJoin<T, U, V, K>(
     inner: Array<U>,
     outerKeySelector: ((x: T) => K),
@@ -30,27 +30,27 @@ interface Array<T> {
     equalityComparer: IEqualityComparer<T>
   );
   Last<T>(): T;
-  Min<number>(): number;
+  Min(): number;
   Min<T>(xform: (x: T) => number): T;
-  Max<number>(): number;
+  Max(): number;
   Max<T>(xform: (x: T) => number): T;
   Prepend<T>(element: T): Array<T>;
   Reverse<T>(): Array<T>;
   Select<T, U>(xform: (x: T) => U): Array<U>;
-  Sum<number>(): number;
-  TakeWhile<T>(predicate: (X: T) => boolean): Array<T>;
+  Sum(): number;
+  TakeWhile<T>(predicate: (x: T) => boolean): Array<T>;
   ToDictionary<T, U, V>(
     keySelector: ((x: T) => U),
     elementSelector: ((x: T) => V)
   ): Dictionary<U, V>;
   Union<T>(adder: Array<T>): Array<T>;
-  Where<T>(predicate: (X: T) => boolean): Array<T>;
-  Single<T>(predicate: (X: T) => boolean): T;
+  Where<T>(predicate: (x: T) => boolean): Array<T>;
+  Single<T>(predicate: (x: T) => boolean): T;
   Contains<T>(element: T): boolean;
-  SkipWhile<T>(predicate: (X: T) => boolean): Array<T>;
+  SkipWhile<T>(predicate: (x: T) => boolean): Array<T>;
 }
 
-interface Array {
+interface ArrayConstructor {
   Empty(): Array<any>;
   Repeat<T>(element: T, count: Number): Array<T>;
 }
